12e6c3c5d6fa08827f36e5d12e4fcf2b9f912204
Weisz Roland <rolika@nostromo>
2024-06-23 16:31:49 +0200

    display calculated values

--- scripts/gui/displaymask.py
+++ scripts/gui/displaymask.py
@@ -2,13 +2,14 @@ from tkinter import *
 from tkinter import ttk
 
 
-
 MID_FIELD = 16
 PADX = 3
 PADY = 3
 
+ALL_SELECTED = "teljes raktár"
 
-class Display(LabelFrame):
+
+class DisplayMask(LabelFrame):
     def __init__(self):
         super().__init__(text="Számított adatok")
         self._init_controll_variables()
@@ -38,3 +39,15 @@ class Display(LabelFrame):
             .grid(row=2, column=1, padx=PADX, pady=PADY)
         Label(self, text="Ft")\
             .grid(row=2, column=2, sticky=W, padx=PADX, pady=PADY)
+    
+    def update_(self, 
+                articlenumber:int, 
+                selectiontext:str, 
+                selectionvalue:float) -> None:
+        self.articlenumber_var.set(f"{articlenumber:06}")
+        if selectiontext:
+            selectiontext = f"""\"{selectiontext}\""""
+        else:
+            selectiontext = ALL_SELECTED
+        self.selectiontext_var.set(selectiontext)
+        self.selectionvalue_var.set(f"{round(selectionvalue):n}")
--- scripts/gui/gui.py
+++ scripts/gui/gui.py
@@ -1,8 +1,7 @@
 import os
 from tkinter import *
 
-from scripts.gui import styles
-from scripts.gui.display import Display
+from scripts.gui.displaymask import DisplayMask
 from scripts.gui.itemmask import ItemMask
 from scripts.gui.stockitemmask import StockItemMask
 
@@ -32,12 +31,13 @@ class Gui(Frame):
     def _build_interface(self):
         self.itemmask = ItemMask()
         self.stockitemmask = StockItemMask()
-        self.display = Display()
+        self.displaymask = DisplayMask()
+        self.displaymask.update_(1, "", 8912234.35)
 
     def _update_labels(self, _:Event):
         self.stockitemmask.unit_var.set(self.itemmask.unit_var.get())
         self.stockitemmask.value_var.set(self.stockitemmask.retrieve().value_fmt)
-        print(styles.is_entry_ok(self.stockitemmask))
+        self.displaymask.update_(1, "valami", 8912234.35)
 
 
 if __name__ == "__main__":
