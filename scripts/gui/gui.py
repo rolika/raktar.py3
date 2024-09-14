addc166bdcb1ce2be60e25c4bd6536b97aac7a58
Weisz Roland <rolika@nostromo>
2024-09-14 14:41:52 +0200

    interface for modifying a stockitem

--- scripts/gui/gui.py
+++ scripts/gui/gui.py
@@ -2,6 +2,7 @@ import os
 from tkinter import *
 
 from scripts.gui.displaymask import DisplayMask
+from scripts.gui.itemlist import ItemList
 from scripts.gui.itemmask import ItemMask
 from scripts.gui.stockitemmask import StockItemMask
 
@@ -32,19 +33,22 @@ class Gui(Frame):
         self.displaymask = DisplayMask()
         self.stockitemmask = StockItemMask()
         self.itemmask = ItemMask()
-        self.displaymask.update_(1, "", 8912234.35)
-        self.displaymask.grid(padx=PADX, pady=PADY, sticky=E+W)
-        self.itemmask.grid(padx=PADX, pady=PADY, sticky=E+W)
-        self.stockitemmask.grid(padx=PADX, pady=PADY, sticky=E+W)
+        self.itemlist = ItemList()
+        self.displaymask.grid(row=0, column=0, padx=PADX, pady=PADY, sticky=E+W)
+        self.itemmask.grid(row=1, column=0, padx=PADX, pady=PADY, sticky=E+W)
+        self.stockitemmask.grid(row=2, column=0, padx=PADX, pady=PADY, sticky=E+W)
+        self.itemlist.grid(row=0, column=1, rowspan=3, padx=PADX, pady=PADY, sticky=N+S)
 
     def _update_labels(self, _:Event):
         self.stockitemmask.unit_var.set(self.itemmask.unit_var.get())
         self.stockitemmask.value_var.set(self.stockitemmask.retrieve().value_fmt)
-        self.displaymask.update_(1, "valami", 8912234.35)
 
 
 if __name__ == "__main__":
     import locale
     locale.setlocale(locale.LC_ALL, "")
     gui = Gui()
+    gui.displaymask.update_(1, "valami", 8912234.35)
+    test_list = [str(i) for i in range(50)]
+    gui.itemlist.populate(test_list)
     gui.mainloop()
--- scripts/gui/itemlist.py
+++ scripts/gui/itemlist.py
@@ -7,11 +7,54 @@ class ItemList(LabelFrame):
         super().__init__(text=title)
         self._init_controll_variables()
         self._build_interface()
-    
+        self._master_list = None
+        self._item_list = None
+
     def _init_controll_variables(self) -> None:
         self.filter_var = StringVar()
         self.list_var = StringVar()
-    
+
     def _build_interface(self) -> None:
-        self.filter_entry = ttk.Entry()
-        self.list_box = Listbox()
+        filter_for = self.register(self._filter_for)
+        self.filter_entry = ttk.Entry(self,
+                                      validate="all",
+                                      validatecommand=(filter_for, "%P"))
+
+        vertical_scroll = Scrollbar(self, orient=VERTICAL)
+        self.list_box = Listbox(self,
+                                cursor="hand2",
+                                listvariable=self.list_var,
+                                selectmode=SINGLE,
+                                width=60,
+                                height=18,
+                                yscrollcommand=vertical_scroll.set)
+        vertical_scroll["command"]=self.list_box.yview
+
+        self.list_box.bind("<Button-4>",
+                           lambda _: self.list_box.yview_scroll(-1, UNITS))
+        self.list_box.bind("<Button-5>",
+                           lambda _: self.list_box.yview_scroll(1, UNITS))
+        self.list_box.bind("<MouseWheel>",
+            lambda e: self.list_box.yview_scroll(int(e.delta / 120), UNITS))
+
+        self.filter_entry.grid(row=0, column=0, columnspan=2, sticky=E+W)
+        self.list_box.grid(row=1, column=0)
+        vertical_scroll.grid(row=1, column=1, sticky=N+S)
+
+    def populate(self, item_list:list) -> None:
+        self._master_list = list(item_list)
+        for i, item in enumerate(item_list):
+            self.list_box.insert(i, str(item))
+
+    def _filter_for(self, text:str) -> bool:
+        self.list_box.delete(0, END)
+        for i, item in enumerate(self._master_list):
+            if text in str(item):
+                self.list_box.insert(i, str(item))
+        return True
+
+
+if __name__ == "__main__":
+    itemlist = ItemList()
+    itemlist.grid()
+    itemlist.mainloop()
