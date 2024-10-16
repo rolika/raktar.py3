832d403615c21274c8bd8d425032838ccb6d35e4
Weisz Roland <rolika@nostromo>
2024-10-16 18:52:37 +0200

    check entry being a number

--- scripts/gui/asklocalfloat.py
+++ scripts/gui/asklocalfloat.py
@@ -4,14 +4,9 @@ locale.setlocale(locale.LC_ALL, "")
 from tkinter import *
 from tkinter import ttk
 from tkinter import simpledialog
-from tkinter import messagebox
 
 from scripts.gui import styles
 
-
-def ask_local_float() -> float:
-    num = None
-    
 
 
 class AskLocalFloat(simpledialog.Dialog):
@@ -19,14 +14,14 @@ class AskLocalFloat(simpledialog.Dialog):
     def __init__(self, title:str, prompt:str, root=None, initvalue:float=None,
                  minvalue:float=None, maxvalue:float=None) -> None:
         self.__prompt = prompt
-        self.__minvalue = minvalue if minvalue else 0
-        self.__maxvalue = maxvalue if maxvalue else initvalue
+        self.__minvalue = minvalue
+        self.__maxvalue = maxvalue
         self.__number = None
         self.__entry_var = StringVar()
         if initvalue:
             self.__entry_var.set(initvalue)
         super().__init__(root, title=title)
-    
+
     def body(self, root:Widget) -> Widget:
         is_okay = self.register(self._is_okay)
         box = Frame(self)
@@ -41,33 +36,35 @@ class AskLocalFloat(simpledialog.Dialog):
     def buttonbox(self):
         """Override standard button texts."""
         box = Frame(self)
-        ttk.Button(box, text="KÉSZ", width=10, command=self.ok, default=ACTIVE)\
-            .pack(side=LEFT, padx=5, pady=5)
-        ttk.Button(box, text="Mégse", width=10, command=self.cancel)\
+        self.__ok_button = Button(box, text="OK", width=10,
+                                  command=self.ok, default=ACTIVE)
+        self.__ok_button.pack(side=LEFT, padx=5, pady=5)
+        Button(box, text="Mégse", width=10, command=self.cancel)\
             .pack(side=LEFT, padx=5, pady=5)
         self.bind("<Return>", self.ok)
         self.bind("<KP_Enter>", self.ok)
         self.bind("<Escape>", self.cancel)
         box.pack()
-    
+
     def apply(self) -> None:
         value = self.__entry_var.get()
         if value:
-            self.__number = locale.atof(value)
-        
+            self.__number = float(value)
+
     @property
     def number(self) -> float:
         return self.__number
-    
+
     def _is_okay(self, text:str, name:str) -> bool:
+        number = None
         try:
-            number = locale.atof(text)
-            if self.__minvalue <= number <= self.__maxvalue:
-                styles.apply_entry_ok(self, name)
+            number = float(text)
+            styles.apply_entry_ok(self, name)
+            self.__ok_button["state"] = NORMAL
         except ValueError:
             styles.apply_entry_error(self, name)
+            self.__ok_button["state"] = DISABLED
         return True
-
 
 if __name__ == "__main__":
     value = AskLocalFloat(title="Kivét", prompt="Mennyit veszel ki?",
