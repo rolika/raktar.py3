9c54a3df5565d31d821446b258db1cdab2452276
Weisz Roland <rolika@nostromo>
2024-10-19 12:49:37 +0200

    create the title interface

---
+++ scripts/gui/title_ui.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import ttk
+
+TITLE_IMAGE = r"data/titleimg.gif"
+
+
+class TitleUI(Frame):
+    def __init__(self, root=None, title_image=TITLE_IMAGE, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.__title_image = PhotoImage(file=title_image)
+        self._body()
+
+    def _body(self) -> None:
+        box = LabelFrame(self, text="Raktárkészlet-kezelő alkalmazás",
+                         labelanchor=N)
+        canvas = Canvas(box, width=640, height=295)
+        canvas.create_image(0, 0, image=self.__title_image, anchor=NW)
+        canvas.pack(padx=5, pady=5)
+        box.pack(padx=5, pady=5)
+        box = ttk.LabelFrame(self, text="Anyagok kezelése")
+        ttk.Button(box, text="Új anyag").pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Meglévő anyag módosítása")\
+            .pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Anyag törlése").pack(fill=X, padx=5, pady=5)
+        box.pack(side=LEFT, fill=BOTH, padx=5, pady=5)
+        box = ttk.LabelFrame(self, text="Raktárkészlet-kezelés")
+        ttk.Button(box, text="Kivét projektre").pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Visszavét projektről")\
+            .pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Új raktári tétel").pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Meglévő raktári tétel törlése")\
+            .pack(fill=X, padx=5, pady=5)
+        ttk.Button(box, text="Raktárkészlet exportálása")\
+            .pack(fill=X, padx=5, pady=5)
+        box.pack(fill=BOTH, padx=5, pady=5)
+
+
+if __name__ == "__main__":
+    app = Tk()
+    titleui = TitleUI(app)
+    titleui.pack()
+    app.resizable(False, False)
+    app.mainloop()
